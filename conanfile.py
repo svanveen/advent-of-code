2984c115bb721eee0a545d4eb583fe1eb76c0366
Stephan van Veen <vanveen.stephan@gmail.com>
2020-12-02 18:30:21 +0100

    Add basic structure

---
+++ conanfile.py
@@ -0,0 +1,44 @@
+from conans import CMake, ConanFile
+
+class AdventOfCodeConan(ConanFile):
+    name = 'advent-of-code'
+    version = '2020.12.01'
+    author = 'svanveen'
+
+    default_user = 'svanveen'
+    default_channel = 'stable'
+
+    scm = {
+        "type": "git",
+        "url": "auto",
+        "revision": "auto",
+    }
+
+    generators = 'cmake'
+    settings = 'os', 'compiler', 'build_type', 'arch'
+
+    default_options = {
+        "cmake:with_openssl": False
+    }
+
+    build_requires = [
+        'cmake/[>=3.16]'
+    ]
+
+    requires = [
+        "range-v3/0.11.0"
+    ]
+
+    no_copy_sources = True
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def deploy(self):
+        self.copy('*')
