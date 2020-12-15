2050ff439bfbffab9e83e0cce806e4c9b333a388
Stephan van Veen <vanveen.stephan@gmail.com>
2020-12-15 22:55:09 +0100

    Add solution for day 6 part 2 in 2019

--- src/lib/src/2019/exercise06.cpp
+++ src/lib/src/2019/exercise06.cpp
@@ -30,18 +30,21 @@ struct Connection
 struct Node
 {
     explicit Node(const Object& orbit)
-        : orbit(orbit)
+        : object(orbit)
     {}
 
-    Object orbit;
+    Object object;
+    Node* parent;
     std::vector<std::unique_ptr<Node>> children;
 };
 
 struct Tree
 {
-    template <typename CONNECTIONS>
-    Tree(CONNECTIONS&& connections)
+    Tree(std::istream& stream)
     {
+        auto connections = ranges::getlines(stream)
+            | ranges::views::transform(Connection::parse);
+
         std::map<Object, std::unique_ptr<Node>> nodes;
         std::map<Object, Node*> addressMap;
         for (auto&& connection : connections)
@@ -60,6 +63,7 @@ struct Tree
             {
                 child = std::make_unique<Node>(connection.child);
             }
+            child->parent = parent;
             addressMap[connection.child] = child.get();
             parent->children.emplace_back(std::move(child));
         }
@@ -68,6 +72,28 @@ struct Tree
             | ranges::views::values
             | ranges::views::filter([](auto&& nodePtr) -> bool { return nodePtr.operator bool(); });
         root = std::move(ranges::front(rootNodes));
+    }
+
+    std::pair<const Node*, int> findNode(const Object& object, const Node& node, int depth = 0)
+    {
+        if (node.object == object)
+        {
+            return std::pair{&node, depth};
+        }
+        for (const auto& child : node.children)
+        {
+            auto childResult = findNode(object, *child, depth + 1);
+            if (childResult.first)
+            {
+                return childResult;
+            }
+        }
+        return std::pair{nullptr, 0};
+    }
+
+    auto findNode(const Object& object)
+    {
+        return findNode(object, *root);
     }
 
     std::unique_ptr<Node> root;
@@ -87,10 +113,7 @@ std::size_t countOrbits(const Node& node, std::size_t distance = 0)
 template <>
 std::size_t exercise<2019, 6, 1>(std::istream& stream)
 {
-    auto connections = ranges::getlines(stream)
-        | ranges::views::transform(Connection::parse);
-
-    auto tree = Tree(connections);
+    auto tree = Tree{stream};
 
     return countOrbits(*tree.root);
 }
@@ -98,7 +121,31 @@ std::size_t exercise<2019, 6, 1>(std::istream& stream)
 template <>
 std::size_t exercise<2019, 6, 2>(std::istream& stream)
 {
-    return 0;
+    auto tree = Tree{stream};
+
+    auto you = tree.findNode("YOU");
+    auto san = tree.findNode("SAN");
+
+    auto parent = [](auto&& pair) { return std::pair{pair.first->parent, pair.second - 1}; };
+
+    int connections = 0;
+    for (; you.first != san.first; )
+    {
+        const auto depthYou = you.second;
+        const auto depthSan = san.second;
+        if (depthYou <= depthSan)
+        {
+            san = parent(san);
+            ++connections;
+        }
+        if (depthYou >= depthSan)
+        {
+            you = parent(you);
+            ++connections;
+        }
+    }
+
+    return connections - 2; // Remove first connection on both ends
 }
 
 }
--- test/src/2019/exercise06.cpp
+++ test/src/2019/exercise06.cpp
@@ -2,7 +2,9 @@
 #include <test/util.h>
 #include <aoc/exercises.h>
 
-constexpr auto input = R"(COM)B
+TEST(Exercise6, Part1)
+{
+    constexpr auto input = R"(COM)B
 B)C
 C)D
 D)E
@@ -14,9 +16,25 @@ E)J
 J)K
 K)L
 )";
-
-TEST(Exercise6, Part1)
-{
     EXPECT_EQ(42, solve(aoc::exercise<2019, 6, 1>, input));
+}
+
+TEST(Exercise6, Part2)
+{
+    constexpr auto input = R"(COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN
+)";
+    EXPECT_EQ(4, solve(aoc::exercise<2019, 6, 2>, input));
 }
 
