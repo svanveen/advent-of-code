d6747dbada1615a8065f8e1fe7905cf97495ec28
Stephan van Veen <vanveen.stephan@gmail.com>
2020-12-18 20:01:00 +0100

    Add token parser

--- src/lib/src/2020/exercise18.cpp
+++ src/lib/src/2020/exercise18.cpp
@@ -10,11 +10,42 @@ namespace aoc
 
 namespace
 {
+
+using Operation = std::variant<std::plus<>, std::multiplies<>>;
+
+struct OpeningBrace{};
+struct ClosingBrace{};
+
+using Token = std::variant<int, Operation, OpeningBrace, ClosingBrace>;
+
+auto parseTokens(const std::string& str)
+{
+    auto parseToken = [](auto&& part) -> Token
+    {
+        switch (*part.first)
+        {
+            case '+': return std::plus<>{};
+            case '*': return std::multiplies<>{};
+            case '(': return OpeningBrace{};
+            case ')': return ClosingBrace{};
+            default:  return std::stoi(part);
+        }
+    };
+
+    auto tokens = str
+        | ranges::views::tokenize(std::regex{R"((\d+|[*+()]))"})
+        | ranges::views::transform(parseToken);
+
+    return 0;
+}
+
 }
 
 template <>
 std::size_t exercise<2020, 18, 1>(std::istream& stream)
 {
+    ranges::getlines(stream)
+        | ranges::views::transform(parseTokens);
     return 0;
 }
 
