44335baf1634cfa39f899ebc4d44204bbe90db1b
Stephan van Veen <vanveen.stephan@gmail.com>
2020-12-23 13:49:15 +0100

    Add solution for day 23 part 2

--- src/lib/src/2020/exercise23.cpp
+++ src/lib/src/2020/exercise23.cpp
@@ -12,7 +12,7 @@ namespace
 
 struct Game
 {
-    explicit Game(std::istream& stream)
+    explicit Game(std::istream& stream, std::optional<int> fillUpTo = {})
         : cups([&]()
             {
                 std::string line;
@@ -20,19 +20,38 @@ struct Game
                 auto cupsRng = line
                     | ranges::views::tokenize(std::regex{R"(\d)"})
                     | ranges::views::transform([](auto&& number) { return std::stoi(number); });
+                if (fillUpTo)
+                {
+                    auto extendedCupsRng = ranges::views::concat(
+                        cupsRng,
+                        ranges::views::closed_iota(ranges::max(cupsRng) + 1, *fillUpTo)
+                    );
+                    return std::list<int>(extendedCupsRng.begin(), extendedCupsRng.end());
+                }
                 return std::list<int>(cupsRng.begin(), cupsRng.end());
             }())
         , min(ranges::min(cups))
         , max(ranges::max(cups))
-    {}
-
-    int operator()(int rounds)
     {
-        for (int move = 0; move < rounds; ++move)
+        for (auto it = cups.begin(); it != cups.end(); ++it)
+        {
+            iteratorMap.emplace(*it, it); // this only works since std::list doesn't invalidate it's iterators on any change
+        }
+    }
+
+    const std::list<int>& getCups()
+    {
+        return cups;
+    }
+
+    void play(int rounds)
+    {
+        for (std::size_t move = 0; move < rounds; ++move)
         {
             auto current = cups.begin();
             const auto value = *current;
-            auto destination = findDestinationCup(std::next(current, 4), cups.end(), value);
+            const auto destinationValue = findDestinationCupValue(std::next(current), std::next(current, 4), value);
+            const auto destination = iteratorMap[destinationValue];
 
             cups.splice(std::next(destination), cups, std::next(current), std::next(current, 4)); // move next 3 behind destination
             cups.splice(cups.end(), cups, current); // move first element to the end
@@ -41,27 +60,25 @@ struct Game
         {
             cups.splice(cups.begin(), cups, ranges::find(cups, 1), cups.end()); // circle 1 up front
         }
-        std::stringstream stream;
-        std::copy(std::next(cups.begin()), cups.end(), std::ostream_iterator<int>(stream));
-        return std::stoi(stream.str());
     }
 
 private:
     template <typename It>
-    It findDestinationCup(It start, It end, int value)
+    int findDestinationCupValue(It start, It end, int value)
     {
         while (true)
         {
             value = (value == min) ? max : value - 1;
-            if (auto it = std::find(start, end, value); it != end)
+            if (std::find(start, end, value) == end)
             {
-                return it;
+                return value;
             }
         }
     }
 
 private:
     std::list<int> cups;
+    std::map<int, std::list<int>::iterator> iteratorMap;
     int min;
     int max;
 };
@@ -74,14 +91,22 @@ template <>
 std::size_t exercise<2020, 23, 1>(std::istream& stream)
 {
     Game game{stream};
+    game.play(100);
 
-    return game(100);
+    const auto& cups = game.getCups();
+    std::stringstream sstream;
+    std::copy(std::next(cups.begin()), cups.end(), std::ostream_iterator<int>(sstream));
+    return std::stoi(sstream.str());
 }
 
 template <>
 std::size_t exercise<2020, 23, 2>(std::istream& stream)
 {
-    return 0;
+    Game game{stream, 1'000'000};
+    game.play(10'000'000);
+
+    const auto& cups = game.getCups();
+    return static_cast<std::size_t>(*std::next(cups.begin())) * static_cast<std::size_t>(*std::next(cups.begin(), 2));
 }
 
 }
--- test/src/2020/exercise23.cpp
+++ test/src/2020/exercise23.cpp
@@ -9,3 +9,8 @@ TEST(Exercise23, Part1)
     EXPECT_EQ(67384529, solve(aoc::exercise<2020, 23, 1>, input));
 }
 
+TEST(Exercise23, Part2)
+{
+    EXPECT_EQ(149245887792, solve(aoc::exercise<2020, 23, 2>, input));
+}
+
