bbe9ab6d90f9c84237a46204345e1cabc4c92bd4
Stephan van Veen <vanveen.stephan@gmail.com>
2021-11-09 12:14:03 +0100

    Attempt exercise 10 part 2 in 2019

--- src/src/2019/Exercise10.cpp
+++ src/src/2019/Exercise10.cpp
@@ -12,6 +12,12 @@ namespace
 using Position = utils::Position<int, 2>;
 using Direction = utils::Direction<int, 2>;
 
+Direction reduce(const Direction& dir)
+{
+    const auto gcd = std::gcd(dir[0], dir[1]);
+    return {{dir[0] / gcd, dir[1] / gcd}};
+}
+
 struct Asteriod
 {
     Asteriod(int col, int row)
@@ -72,6 +78,60 @@ Result exercise<2019, 10, 1>(std::istream& stream)
 template <>
 Result exercise<2019, 10, 2>(std::istream& stream)
 {
+    const auto asteroids = findAsteroids(stream);
+
+    constexpr auto lessCount = [](const auto& lhs, const auto& rhs)
+    {
+        return lhs.count < rhs.count;
+    };
+    const auto& laserPosition = ranges::max(asteroids, lessCount).position;
+
+    auto asteroidPositions = std::set<Position>{};
+    for (const auto& asteroid : asteroids)
+    {
+        asteroidPositions.emplace(asteroid.position);
+    }
+
+    const auto maxCol = ranges::max(asteroids | ranges::views::transform([](const auto& asteroid) { return asteroid.position[0]; }));
+    const auto maxRow = ranges::max(asteroids | ranges::views::transform([](const auto& asteroid) { return asteroid.position[1]; }));
+
+    auto count = 0;
+    const auto border = ranges::views::zip
+    (
+        ranges::views::concat
+        (
+            ranges::views::closed_iota(laserPosition[0], maxCol),
+            ranges::views::repeat_n(maxCol, maxRow - 1),
+            ranges::views::closed_iota(0, maxCol) | ranges::views::reverse,
+            ranges::views::repeat_n(0, maxRow - 1),
+            ranges::views::iota(0, laserPosition[0])
+        ),
+        ranges::views::concat
+        (
+            ranges::views::repeat_n(0, maxCol - laserPosition[0]),
+            ranges::views::closed_iota(0, maxRow),
+            ranges::views::repeat_n(maxRow, maxCol - 1),
+            ranges::views::closed_iota(0, maxRow) | ranges::views::reverse,
+            ranges::views::repeat_n(0, laserPosition[0] - 1)
+        )
+    ) | ranges::views::transform([](const auto& coord) { return Position{{std::get<0>(coord), std::get<1>(coord)}}; });
+    for (const auto& pos : border)
+    {
+        const auto direction = pos - laserPosition;
+        auto reducedDirection = reduce(direction);
+        for (auto dir = reducedDirection; dir != direction + reducedDirection; dir += reducedDirection)
+        {
+            auto it = std::find(asteroidPositions.begin(), asteroidPositions.end(), laserPosition + dir);
+            if (it != asteroidPositions.end())
+            {
+                std::cout << *it << std::endl;
+                asteroidPositions.erase(it);
+                ++count;
+                break;
+            }
+        }
+    }
+
     return 0;
 }
 
